9e239be1308baa877dd3f1127062ac6351065f85
josiesam <josiesam369@gmail.com>
2026-07-08 22:19:05 +0000

    fix(navbar): letter text

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -35,7 +35,7 @@ export default function Navbar() {
             alt=""
             width={150}
             height={40}
-            className="-ml-12 font-black xl:hidden text-blue-700 tracking-tighter font-headline"
+            className=" font-black xl:hidden text-blue-700 tracking-tighter font-headline"
           />
         </Link>
 
