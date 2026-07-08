36fab897d321eceff4647cdf4dafcd39b7bc3963
josiesam <josiesam369@gmail.com>
2026-07-08 22:25:50 +0000

    fix(navbar): letter text

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -33,7 +33,7 @@ export default function Navbar() {
           <Image
             src="/ysd-text.png"
             alt=""
-            width={120}
+            width={100}
             height={40}
             className=" font-black xl:hidden text-blue-700 tracking-tighter font-headline"
           />
